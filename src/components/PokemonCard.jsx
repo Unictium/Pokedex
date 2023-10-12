c53af178c0f4e2f90984a56df66177b12b99bbd1
Unictium <145552529+Unictium@users.noreply.github.com>
2023-10-12 09:03:33 +0200

    Update PokemonCard.jsx

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,41 +1,18 @@
 import React from 'react';
 
-const PokemonCard = ({ name, imageUrl }) => {
+const PokemonCard = ({ name, imageUrl}) => {
+    console.log(name);
+    console.log(imageUrl);
   return (
     <div className="pokemon-card">
       <figure>
-        {imageUrl ? (
-          <img src={imageUrl} alt={name} />
-        ) : (
-          <p>???</p>
-        )}
+        <img 
+        src={imageUrl}
+        alt={name} />
         <figcaption>{name}</figcaption>
       </figure>
     </div>
   );
 };
 
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-    imgSrc:
-    "mew.png",
-  },
-];
-
-const App = () => {
-  const pokemon = pokemonList[1]; 
-
-  return (
-    <div className="App">
-      <PokemonCard name={pokemon.name} imageUrl={pokemon.imgSrc} />
-    </div>
-  );
-};
-
-export default App;
+export default PokemonCard;
